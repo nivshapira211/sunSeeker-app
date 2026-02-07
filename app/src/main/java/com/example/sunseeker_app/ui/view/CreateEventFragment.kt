bd153cd91201f0e7cec8edf9248556d0604807e9
ofekSaar <ofeksaar87@gmail.com>
2026-02-07 14:18:16 +0200

    Add navigation graph and UI skeleton

---
+++ app/src/main/java/com/example/sunseeker_app/ui/view/CreateEventFragment.kt
@@ -0,0 +1,6 @@
+package com.example.sunseeker_app.ui.view
+
+import androidx.fragment.app.Fragment
+import com.example.sunseeker_app.R
+
+class CreateEventFragment : Fragment(R.layout.fragment_create_event)
---
+++ app/src/main/java/com/example/sunseeker_app/ui/view/LoginFragment.kt
@@ -0,0 +1,6 @@
+package com.example.sunseeker_app.ui.view
+
+import androidx.fragment.app.Fragment
+import com.example.sunseeker_app.R
+
+class LoginFragment : Fragment(R.layout.fragment_login)
